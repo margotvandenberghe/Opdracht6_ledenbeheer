22c8e5e22f5d14aec2b54e1847f1ef38e0d427c3
student <student@gebruikers-mac-mini-2.local>
2018-11-21 09:39:02 +0100

    Initial Commit

---
+++ Opdracht6_ledenbeheer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Opdracht6_ledenbeheer
+//
+//  Created by student on 21/11/2018.
+//  Copyright © 2018 Vives. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
